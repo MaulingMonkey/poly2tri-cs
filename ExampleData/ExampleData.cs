3c4b915d424fe8781d4e66cef565ca846b5d9b58
Michael B. E. Rickert <nobody@nowhere.noplace>
2010-02-21 04:19:35 -0800

    Started working on examples

---
+++ ExampleData/ExampleData.cs
@@ -0,0 +1,89 @@
+﻿/* Poly2Tri
+ * Copyright (c) 2009-2010, Poly2Tri Contributors
+ * http://code.google.com/p/poly2tri/
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of Poly2Tri nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+using System;
+
+namespace Poly2Tri {
+	public static class ExampleData {
+		public static List<PolygonPoint> LoadDat( string filename ) {
+			var points = new List<PolygonPoint>();
+			foreach ( var line_ in File.ReadAllLines(filename) ) {
+				string line = line_.Trim();
+				if ( string.IsNullOrEmpty(line) ) continue;
+				var xy = line.Split( new[]{' ',','}, StringSplitOptions.RemoveEmptyEntries );
+				points.Add( new PolygonPoint( double.Parse(xy[0]), double.Parse(xy[1]) ) );
+			}
+			return points;
+		}
+
+		static readonly Dictionary<string,List<PolygonPoint>> DatCache = new Dictionary<string,List<PolygonPoint>>();
+
+		static List<PolygonPoint> CacheLoadDat( string filename ) {
+			if (!DatCache.ContainsKey(filename)) DatCache.Add( filename, LoadDat(filename) );
+			return DatCache[filename];
+		}
+
+		static readonly Dictionary<string,Image> ImageCache = new Dictionary<string,Image>();
+
+		static Image CacheLoadImage( string filename ) {
+			if (!ImageCache.ContainsKey(filename)) ImageCache.Add( filename, new Bitmap(filename) );
+			return ImageCache[filename];
+		}
+
+		public static List<PolygonPoint> Two         { get { return CacheLoadDat(@"Data\2.dat"); } }
+		public static List<PolygonPoint> Bird        { get { return CacheLoadDat(@"Data\bird.dat"); } }
+		public static List<PolygonPoint> Custom      { get { return CacheLoadDat(@"Data\custom.dat"); } }
+		public static List<PolygonPoint> Debug       { get { return CacheLoadDat(@"Data\debug.dat"); } }
+		public static List<PolygonPoint> Debug2      { get { return CacheLoadDat(@"Data\debug2.dat"); } }
+		public static List<PolygonPoint> Diamond     { get { return CacheLoadDat(@"Data\diamond.dat"); } }
+		public static List<PolygonPoint> Dude        { get { return CacheLoadDat(@"Data\dude.dat"); } }
+		public static List<PolygonPoint> Funny       { get { return CacheLoadDat(@"Data\funny.dat"); } }
+		public static List<PolygonPoint> NazcaHeron  { get { return CacheLoadDat(@"Data\nazca_heron.dat"); } }
+		public static List<PolygonPoint> NazcaMonkey { get { return CacheLoadDat(@"Data\nazca_monkey.dat"); } }
+		public static List<PolygonPoint> Sketchup    { get { return CacheLoadDat(@"Data\sketchup.dat"); } }
+		public static List<PolygonPoint> Star        { get { return CacheLoadDat(@"Data\star.dat"); } }
+		public static List<PolygonPoint> Strange     { get { return CacheLoadDat(@"Data\strange.dat"); } }
+		public static List<PolygonPoint> Tank        { get { return CacheLoadDat(@"Data\tank.dat"); } }
+		public static List<PolygonPoint> Test        { get { return CacheLoadDat(@"Data\test.dat"); } }
+
+		public static IEnumerable<List<PolygonPoint>> Polygons { get { return new[]
+			{ Two, Bird, Custom, Debug, Debug2, Diamond, Dude, Funny, NazcaHeron, NazcaMonkey
+			//, Sketchup
+			, Star, Strange, Tank, Test
+			};
+		}}
+
+		public static Image Logo256x256 { get { return CacheLoadImage(@"Textures\poly2tri_logotype_256x256.png"); } }
+	}
+}
--- SwfTest/TestForm.cs
+++ SwfTest/TestForm.cs
@@ -8,35 +8,66 @@ using Poly2Tri;
 
 namespace SwfTest {
 	[System.ComponentModel.DesignerCategory("")] class TestForm : Form {
-		Polygon Monkey;
+		List<Polygon> Polygons;
+
+		int i;
+
+		Timer rotation;
 
 		public TestForm() {
 			ClientSize = new Size(1000, 1000);
 			DoubleBuffered = true;
 			Text = "Just a test";
 			Visible = true;
+
+			Polygons = new List<Polygon>( ExampleData.Polygons.Select( data => new Polygon(data) ) );
+			foreach ( var poly in Polygons ) P2T.Triangulate(poly);
+
+			rotation = new Timer()
+				{ Enabled = true
+				, Interval = 5000
+				};
+			rotation.Tick += (o,e) => {
+				i = (i+1)%Polygons.Count;
+				Invalidate();
+			};
+
 			Invalidate();
-
-			var monkeydata = new List<PointF>();
-			foreach (var line in File.ReadAllLines(@"..\..\nazca_monkey.dat")) {
-				var xy = line.Split(' ');
-				monkeydata.Add(new PointF(float.Parse(xy[0]), float.Parse(xy[1])));
-			}
-
-			Monkey = new Polygon(monkeydata.Select(p => new PolygonPoint(p.X, p.Y)));
-			P2T.Triangulate(Monkey);
 		}
 
 		protected override void OnPaint( PaintEventArgs e ) {
-			var fx = e.Graphics;
-			fx.TranslateTransform(500, 500);
-			fx.ScaleTransform(1, -1);
-			foreach ( var tri in Monkey.Triangles ) fx.DrawPolygon(Pens.Green, new PointF[]
-				{ new PointF(5*tri.Points[0].Xf,5*tri.Points[0].Yf)
-				, new PointF(5*tri.Points[1].Xf,5*tri.Points[1].Yf)
-				, new PointF(5*tri.Points[2].Xf,5*tri.Points[2].Yf)
-				});
+			float xmin = float.MaxValue, xmax = float.MinValue;
+			float ymin = float.MaxValue, ymax = float.MinValue;
+
+			foreach ( var point in Polygons[i].Points ) {
+				xmin = Math.Min(xmin,point.Xf);
+				xmax = Math.Max(xmax,point.Xf);
+				ymin = Math.Min(ymin,point.Yf);
+				ymax = Math.Max(ymax,point.Yf);
+			}
+
+			if ( xmin<xmax && ymin<ymax ) {
+				var fx = e.Graphics;
+				float zoom = 0.8f * Math.Min(ClientSize.Width/(xmax-xmin),ClientSize.Height/(ymax-ymin));
+				fx.TranslateTransform(ClientSize.Width/2, ClientSize.Height/2); // center coordinate system on screen center
+				fx.ScaleTransform(zoom, -zoom);
+				fx.TranslateTransform(-(xmax+xmin)/2,-(ymax+ymin)/2); // center image
+
+				using ( var pen = new Pen( Color.Green, 1.0f/zoom ) ) foreach ( var tri in Polygons[i].Triangles ) fx.DrawPolygon(pen, new PointF[]
+					{ new PointF(tri.Points[0].Xf,tri.Points[0].Yf)
+					, new PointF(tri.Points[1].Xf,tri.Points[1].Yf)
+					, new PointF(tri.Points[2].Xf,tri.Points[2].Yf)
+					});
+				fx.ResetTransform();
+				fx.DrawImage( ExampleData.Logo256x256, ClientSize.Width-256-10, 10, 256, 256 );
+			}
+
 			base.OnPaint(e);
+		}
+
+		protected override void OnResize( EventArgs e ) {
+			Invalidate();
+			base.OnResize(e);
 		}
 
 		[STAThread] static void Main() {
